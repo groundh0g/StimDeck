69133ac8235f0fb177b345fd4e701b066ab83c46
Joseph Hall <joehall@moreoncode.com>
2023-01-04 04:35:53 -0500

    linting fixes

--- stimdeck/electron/main.ts
+++ stimdeck/electron/main.ts
@@ -75,7 +75,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => { console.log("A"); createWindow(); console.log("B"); }) .catch(console.error);
+app.whenReady().then(() => { console.log("A"); createWindow(); console.log("B"); }).catch(console.error);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -95,4 +95,4 @@ app.on('activate', () => {
 // app.on('browser-window-created', (event, win) => { win.maximize(); });
 // app.on('ready', (event, win) => { win.maximize(); });
 
-export default {}
+export { };
