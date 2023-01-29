6982b21268f0d22bdf9a49e6a3c6dc155a0ad6c5
Joseph Hall <joehall@moreoncode.com>
2023-01-29 01:41:03 -0500

    fixes for test and linting errors

--- stimdeck/src/App.test.tsx
+++ stimdeck/src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {SetColumnSizeFunction, SetThemeFunction} from "./components/columns/Types";
 
 test('renders column header and content divs', () => {
-  render(<App theme="light" columnSize="small" />);
+  render(<App
+      theme="light"
+      columnSize="small"
+      setTheme={((self, event, theme) => { /**/ }) as SetThemeFunction}
+      setColumnSize={((self, event, columnSize) => { /**/ }) as SetColumnSizeFunction} />);
 
   const columnHeaderElements = screen.getAllByText(/groundh0g/i);
   const columnContentElements = screen.getAllByText(/Loading .../i);
